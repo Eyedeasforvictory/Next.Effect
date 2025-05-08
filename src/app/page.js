77cf4eda9297642f1ece0ed43844a19e89d648b5
Eyedeasforvictory <user1@mac.local>
2025-05-08 06:13:50 -0700

    Initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,111 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const containerRef = useRef(null);
+  const highlightRef = useRef(null);
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  useEffect(() => {
+    const container = containerRef.current;
+    const highlight = highlightRef.current;
+    const gridItems = container.querySelectorAll(".grid-item");
+    const firstItem = container.querySelector(".grid-item");
+
+    const highlightColors = [
+      "#E24E1B",
+      "#4381C1",
+      "#F79824",
+      "#04A777",
+      "#5B8C5A",
+      "#2176FF",
+      "#818D92",
+      "#22AAA1",
+    ];
+
+    gridItems.forEach((item, index) => {
+      item.dataset.color = highlightColors[index % highlightColors.length];
+    });
+
+    const moveToElement = (element) => {
+      if (element) {
+        const rect = element.getBoundingClientRect();
+        const containerRect = container.getBoundingClientRect();
+
+        highlight.style.transform = `translate(${
+          rect.left - containerRect.left
+        }px, ${rect.top - containerRect.top}px)`;
+        highlight.style.width = `${rect.width}px`;
+        highlight.style.height = `${rect.height}px`;
+        highlight.style.backgroundColor = element.dataset.color;
+      }
+    };
+
+    const moveHighlight = (e) => {
+      const hoveredElement = document.elementFromPoint(e.clientX, e.clientY);
+
+      if (hoveredElement && hoveredElement.classList.contains("grid-item")) {
+        moveToElement(hoveredElement);
+      } else if (
+        hoveredElement &&
+        hoveredElement.parentElement &&
+        hoveredElement.parentElement.classList.contains("grid-item")
+      ) {
+        moveToElement(hoveredElement.parentElement);
+      }
+    };
+
+    moveToElement(firstItem);
+
+    container.addEventListener("mousemove", moveHighlight);
+
+    return () => {
+      container.removeEventListener("mousemove", moveHighlight);
+    };
+  }, []);
+
+  return (
+    <>
+      <nav>
+        <p>Codegrid</p>
+        <p>/ Experiment 448</p>
+      </nav>
+      <div className="container" ref={containerRef}>
+        <div className="grid">
+          <div className="grid-row">
+            <div className="grid-item">
+              <p>( html )</p>
+            </div>
+            <div className="grid-item">
+              <p>( css )</p>
+            </div>
+            <div className="grid-item">
+              <p>( javascript )</p>
+            </div>
+          </div>
+          <div className="grid-row">
+            <div className="grid-item">
+              <p>( gsap )</p>
+            </div>
+            <div className="grid-item">
+              <p>( scrolltrigger )</p>
+            </div>
+            <div className="grid-item">
+              <p>( react )</p>
+            </div>
+            <div className="grid-item">
+              <p>( next.js )</p>
+            </div>
+            <div className="grid-item">
+              <p>( three.js )</p>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        <div className="highlight" ref={highlightRef}></div>
+      </div>
+      <footer>
+        <p>Unlock Source Code with PRO</p>
+        <p>Link in description</p>
       </footer>
-    </div>
+    </>
   );
 }
